cdb2826c3a493dd43cc15a516173d0ae13783408
Reese Benson <ReeseABenson@gmail.com>
2020-08-28 18:51:07 -0700

    Commits now can be saved to DB

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,15 +1,17 @@
 
 create table project (
     id SERIAL Primary Key,
-    title Varchar not null
+    title Varchar not null Unique
 );
 
 create Table commit_history (
     commit_id Varchar Primary Key,
     project_id int not null,
     commiter Varchar,
-    build_success bit,
-    test_success bit,
+    build_success BOOLEAN,
+    test_success BOOLEAN,
+    auto_push_success BOOLEAN,
+    message varchar,
     CONSTRAINT fk_project
       FOREIGN KEY(project_id) 
 	  REFERENCES project(id)
--- src/main/java/project1/reesebenson/DB/Dao/commitDao.java
+++ src/main/java/project1/reesebenson/DB/Dao/commitDao.java
@@ -1,21 +1,58 @@
 package project1.reesebenson.DB.Dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
+import project1.reesebenson.DB.ConnectionUtils.ConnectionUtils;
 import project1.reesebenson.DB.Entities.CommitEntity;
 
 public class CommitDao implements IDao<CommitEntity, String>{
-    
+    final String Table = "commit_history";
     public List<CommitEntity> getAll(int projectId){
         return null;
     }
 
     public CommitEntity get(String id){
-        return null;
+        String sql = "Select * FROM " + Table + " where id = ?";
+        try {
+            PreparedStatement stmt = ConnectionUtils.getConnection().prepareStatement(sql);
+            stmt.setString(1, id);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            String commit_id = rs.getString("commit_id");
+            int project_id = rs.getInt("project_id");
+            String commiter = rs.getString("commiter");
+            Boolean build_success = rs.getBoolean("build_success");
+            Boolean test_success =rs.getBoolean("test_success");
+            String message = rs.getString("message");
+            Boolean auto_push = rs.getBoolean("auto_push_success");
+            CommitEntity entity = new CommitEntity(commit_id, project_id, commiter, build_success, test_success, message, auto_push);
+            return entity;
+        } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public void create(CommitEntity entity){
-        return;
+        String sql = "Insert into " + Table + " (commit_id, project_id, commiter, build_success, test_success, auto_push_success, message) Values (?,?,?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = ConnectionUtils.getConnection().prepareStatement(sql);
+            stmt.setString(1, entity.getCommit_id());
+            stmt.setInt(2, entity.getProject_id());
+            stmt.setString(3, entity.getUser());
+            stmt.setBoolean(4, entity.isBuild_success());
+            stmt.setBoolean(5, entity.isTest_success());
+            stmt.setBoolean(6, entity.isAuto_push());
+            stmt.setString(7, entity.getMessage());
+            stmt.executeUpdate();
+            } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
+            }
+            return;
     }
 
     public void delete(CommitEntity entity){
